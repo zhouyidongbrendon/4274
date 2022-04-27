bbe10babaadb105ec59fcd6a8874c2c09e622a47
zhouyidongbrendon <103399164+zhouyidongbrendon@users.noreply.github.com>
2022-04-27 14:42:00 +0800

    Add files via upload

---
+++ 111.js
@@ -0,0 +1,8 @@
+ 
+<form name="myform" method="post" action="mailto:username@hotmail.com" onsubmit="return validateform()" >  
+Name: <input type="text" name="name"><br/>  
+Password: <input type="password" name="password"><br/> 
+Name: <input type="text" name="name1"><br/>  
+Name: <input type="text" name="name2"><br/>  
+<input type="submit" value="register">  
+</form>  
